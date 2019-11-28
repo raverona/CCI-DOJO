60f3d2cb375f35538d30c5378671566111bc271e
Rafael Marinaro Verona <rverona@uolinc.com>
2019-11-28 15:41:26 -0300

    Add Permuted Strings generator

--- src/test/kotlin/string/CheckPermutationTest.kt
+++ src/test/kotlin/string/CheckPermutationTest.kt
@@ -4,6 +4,7 @@ import io.kotlintest.properties.Gen
 import io.kotlintest.properties.assertAll
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.ShouldSpec
+import kotlin.random.Random.Default.nextInt
 
 class CheckPermutationTest : ShouldSpec() {
     init {
@@ -26,17 +27,42 @@ class CheckPermutationTest : ShouldSpec() {
 
 object PermutedStringsGenerator : Gen<Pair<String, String>> {
     override fun constants(): Iterable<Pair<String, String>> {
-        TODO("not implemented")
+        return listOf("" to "")
     }
 
     override fun random(): Sequence<Pair<String, String>> {
-        TODO("not implemented")
+        return generateSequence { getNextRandomLatinAlphabetString(nextInt(1, 20)).toPairWithPermutation() }
+    }
+
+    private fun getNextLatinAlphabetChar(): Char = nextInt(from = 97, until = 123).toChar()
+
+    private fun getNextRandomLatinAlphabetString(length: Int): String {
+        return (0..length).map { getNextLatinAlphabetChar() }.joinToString("")
+    }
+
+    private fun String.toPairWithPermutation(): Pair<String, String> {
+        return this to this.permuted()
+    }
+
+    private fun String.permuted(): String {
+        val indexesPermuted = mutableSetOf<Int>()
+
+        fun String.permute(randomIndex: (from: Int, to: Int) -> Int): Char {
+            val index = randomIndex(0, this.length)
+            if (indexesPermuted.contains(index)) return this.permute(randomIndex)
+            indexesPermuted.add(index)
+            return this[index]
+        }
+
+        return (this.indices).map {
+            this.permute { from: Int, until: Int -> nextInt(from, until) }
+        }.joinToString("")
     }
 }
 
 object DistinctNonPermutedStringsGenerator : Gen<Pair<String, String>> {
     override fun constants(): Iterable<Pair<String, String>> {
-        TODO("not implemented")
+        return listOf()
     }
 
     override fun random(): Sequence<Pair<String, String>> {
