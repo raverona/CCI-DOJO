4010082da0107d22ae936d85da91b1d49b9058e1
Rafael Marinaro Verona <rverona@uolinc.com>
2019-11-29 12:23:16 -0300

    Add test structure for URLify test

--- src/test/kotlin/string/URLifyTest.kt
+++ src/test/kotlin/string/URLifyTest.kt
@@ -1,9 +1,34 @@
 package string
 
+import io.kotlintest.properties.Gen
+import io.kotlintest.properties.assertAll
+import io.kotlintest.shouldBe
 import io.kotlintest.specs.ShouldSpec
 
 class URLifyTest : ShouldSpec() {
     init {
-
+        should("replace spaces with '%20'") {
+            assertAll(RandomStringWithSpacesGenerator) { randomStringWithSpaces: String ->
+                URLify().URLify(
+                    randomStringWithSpaces,
+                    randomStringWithSpaces.length
+                ) shouldBe randomStringWithSpaces.replaceSpacesWithPercentTwenty()
+            }
+        }
     }
+
+    private fun String.replaceSpacesWithPercentTwenty(): String {
+        return this.trimEnd().replace(" ".toRegex(), "%20")
+    }
+}
+
+object RandomStringWithSpacesGenerator : Gen<String> {
+    override fun constants(): Iterable<String> {
+        return listOf("", " ", "  ", "   ")
+    }
+
+    override fun random(): Sequence<String> {
+        TODO()
+    }
+
 }
