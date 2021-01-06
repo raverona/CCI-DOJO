9f670dca9f524f13aec2253018beca18fbd56ee1
Rafael Verona <raverona@gmail.com>
2021-01-06 18:11:48 -0300

    Implement solution using a hashmap

--- src/main/kotlin/string/IsUnique.kt
+++ src/main/kotlin/string/IsUnique.kt
@@ -2,6 +2,20 @@ package string
 
 class IsUnique {
     fun isUnique(word: String): Boolean {
-        TODO()
+        val characters = hashMapOf<Char, Int>()
+
+        word.forEach { character ->
+            if(characters.containsKey(character))
+                return false
+            characters[character] = 0
+        }
+
+        return true
     }
 }
+
+/*
+* Time Complexity:  O(n)
+* Space Complexity: O(n)
+*
+* n = size of 'word' parameter*/
