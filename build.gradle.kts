0b3d52557ac9edfdaaf89619ea07b1ca253db202
Rafael Verona <raverona@gmail.com>
2021-01-11 18:48:56 -0300

    Remove unused libs

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,9 +15,6 @@ repositories {
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
     testImplementation("io.kotlintest:kotlintest-runner-junit5:3.4.2")
-    testImplementation("io.arrow-kt:arrow-core:0.10.2")
-    testImplementation("io.arrow-kt:arrow-syntax:0.10.2")
-    kapt("io.arrow-kt:arrow-meta:0.10.2")
 }
 
 tasks.withType<KotlinCompile> {
